1fca4919950411ebb1c2b1d5efce07f1d659b096
kazuma5727 <kazuma_yamamoto@namicoh.co.jp>
2023-03-14 16:14:29 +0900

    get_all_itemsの追加とバージョンアップ

--- base_pyfile/__init__.py
+++ base_pyfile/__init__.py
@@ -1,6 +1,12 @@
 from base_pyfile.file_manager import read_text_file, write_file
 from base_pyfile.log_setting import get_log_handler, make_logger
-from base_pyfile.path_manager import get_all_subfolders, get_files, make_directory, unique_path
+from base_pyfile.path_manager import (
+    get_all_items,
+    get_all_subfolders,
+    get_files,
+    make_directory,
+    unique_path,
+)
 
 __all__ = [
     "read_text_file",
@@ -9,6 +15,7 @@ __all__ = [
     "timer",
     "get_log_handler",
     "make_logger",
+    "get_all_items",
     "get_all_subfolders",
     "get_files",
     "make_directory",
--- setup.py
+++ setup.py
@@ -2,8 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="base_pyfile",
-    version="0.0.3",
+    version="0.0.4",
     packages=find_packages(),
     install_requires=["natsort", "tqdm"],
-    # entry_points={"console_scripts": ["my_command=my_package.my_module:main"]},
 )
