caf325e4d866cdeb363fcd1716b407c5f88175fb
kazuma5727 <kazuma_yamamoto@namicoh.co.jp>
2023-03-14 15:00:19 +0900

    バージョンアップ0.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="base_pyfile",
-    version="0.0.2",
+    version="0.0.3",
     packages=find_packages(),
     install_requires=["natsort", "tqdm"],
     # entry_points={"console_scripts": ["my_command=my_package.my_module:main"]},
