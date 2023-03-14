44a4ae0ecdf9788555345eb038d5978aa9fbe854
kamuya093 <assaultkamuya@gmail.com>
2023-03-14 20:58:32 +0900

    requirementsの改修

--- setup.py
+++ setup.py
@@ -1,12 +1,16 @@
+import os
+
 from setuptools import find_packages, setup
+
 import base_pyfile
 
 # mypackage.__version__ を取得する
 version = base_pyfile.__version__
+requirements = base_pyfile.read_text_file("requirements.txt", "\n")
 
 setup(
     name="base_pyfile",
     version=version,
     packages=find_packages(),
-    install_requires=["natsort", "tqdm"],
+    install_requires=requirements,
 )
