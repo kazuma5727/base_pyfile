aa51589fe7e3abcceb14870c9dd2b391f6fe850f
kazuma5727 <kazuma_yamamoto@namicoh.co.jp>
2023-03-15 11:11:36 +0900

    version変更

--- base_pyfile/__init__.py
+++ base_pyfile/__init__.py
@@ -2,7 +2,7 @@ from base_pyfile.file_manager import read_text_file, write_file
 from base_pyfile.function_timer import logger_timer, timer
 from base_pyfile.log_setting import get_log_handler, make_logger
 from base_pyfile.path_manager import (
-    get_all_items,
+    get_all_files,
     get_all_subfolders,
     get_files,
     make_directory,
@@ -16,7 +16,7 @@ __all__ = [
     "timer",
     "get_log_handler",
     "make_logger",
-    "get_all_items",
+    "get_all_files",
     "get_all_subfolders",
     "get_files",
     "make_directory",
@@ -28,4 +28,4 @@ num_modules = len(__all__)
 # バージョン番号を更新
 tens_place = num_modules // 10
 ones_place = num_modules % 10
-__version__ = f"{tens_place}.{ones_place}.1"
+__version__ = f"{tens_place}.{ones_place}.2"
