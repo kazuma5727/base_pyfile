536ec9a2b3d9d8c3ef6eb74cb35105104b7d60b3
kazuma5727 <kazuma_yamamoto@namicoh.co.jp>
2023-03-21 14:38:29 +0900

    pyproject削除

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
-# from setuptools import find_packages, setup
+from setuptools import find_packages, setup
 
-# import base_pyfile
+import base_pyfile
 
-# # mypackage.__version__ を取得する
-# version = base_pyfile.__version__
-# requirements = base_pyfile.read_text_file("requirements.txt", "\n")
+# mypackage.__version__ を取得する
+version = base_pyfile.__version__
+requirements = base_pyfile.read_text_file("requirements.txt", "\n")
 
-# setup(
-#     name="base_pyfile",
-#     version=version,
-#     packages=find_packages(),
-#     install_requires=requirements,
-# )
+setup(
+    name="base_pyfile",
+    version=version,
+    packages=find_packages(),
+    install_requires=requirements,
+)
