2d7ae4f562002bf28ea6f6ee03d29299a85679b8
kazuma5727 <kazuma_yamamoto@namicoh.co.jp>
2023-11-10 14:17:33 +0900

    require変更

--- setup.py
+++ setup.py
@@ -1,19 +1,41 @@
+from pathlib import Path
+
 from setuptools import find_packages, setup
 
 import base_pyfile
 
+# Settings
+FILE = Path(__file__).resolve()
+PARENT = FILE.parent  # root directory
+
+
 # mypackage.__version__ を取得する
 version = base_pyfile.__version__
 
-requirements = []
-with open("requirements.txt", "r", encoding="utf-8") as fh:
-    for line in fh:
+
+def parse_requirements(file_path: Path):
+    """
+    Parse a requirements.txt file, ignoring lines that start with '#' and any text after '#'.
+
+    Args:
+        file_path (str | Path): Path to the requirements.txt file.
+
+    Returns:
+        (List[str]): List of parsed requirements.
+    """
+
+    requirements = []
+    for line in Path(file_path).read_text().splitlines():
         line = line.strip()
-        requirements.append(line)
+        if line and not line.startswith("#"):
+            requirements.append(line.split("#")[0].strip()) 
+
+    return requirements
+
 
 setup(
     name="base_pyfile",
     version=version,
     packages=find_packages(),
-    install_requires=requirements,
+    install_requires=parse_requirements(PARENT / "requirements.txt"),
 )
