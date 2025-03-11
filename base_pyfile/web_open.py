ac3e04e2ee5a6f43b90e3238f00c577c52427ac3
kazuma5727 <kazuma_yamamoto@namicoh.co.jp>
2025-03-11 09:42:28 +0900

    open_page

--- base_pyfile/web_open.py
+++ base_pyfile/web_open.py
@@ -15,52 +15,57 @@ logger = getLogger("log").getChild(__name__)
 logger.addHandler(NullHandler())
 
 
-def open_page(url: str, delay: int = 2, sumaho: bool = False, paste=False) -> int:
+def open_page(urls: list, delay: int = 2, sumaho: bool = False, paste=False) -> int:
     """
     指定されたURLを開き、一定の遅延時間の後に開いたページの数を返します。
 
     Args:
-        url (str): 開くURL。
+        urls (str): 開くURL。
         delay (int, optional): ページを開いた後の遅延時間（秒）。デフォルトは2秒。
 
     Returns:
         int: 開いたページの数（常に1）。
     """
-    web_count = 0  # 開いたウェブページの数をカウントする変数
-    webbrowser.open(url)  # 指定されたURLをブラウザで開く
-    web_count += 1  # 開いたページの数をインクリメント
-    time.sleep(delay)  # 指定された遅延時間だけ待機
-    if sumaho:
-        pyautogui.press("F12")
-        time.sleep(2)
-        if not search_color(59, 59, 63, 122, 122):
-            move_and_click(2050, 130)
-            time.sleep(2)
-        pyautogui.press("F5")
-        time.sleep(delay)  # 指定された遅延時間だけ待機
-        if paste == True:
-            pyperclip.copy(url)
-            move_and_click(1270, 60)
-            pyautogui.hotkey("ctrl", "a")
-            time.sleep(0.5)
-            pyautogui.hotkey("ctrl", "v")
-            pyautogui.press("enter")
+    if isinstance(urls, str):
+        urls = [urls]
+    for url in urls:
+        logger.debug(f"open page: {url}")
+        try:
+            webbrowser.open(url)  # 指定されたURLをブラウザで開く
+            time.sleep(delay)  # 指定された遅延時間だけ待機
+            if sumaho:
+                pyautogui.press("F12")
+                time.sleep(2)
+                if not search_color(59, 59, 63, 122, 122):
+                    move_and_click(2050, 130)
+                    time.sleep(2)
+                pyautogui.press("F5")
+                time.sleep(delay)  # 指定された遅延時間だけ待機
+                if paste == True:
+                    pyperclip.copy(url)
+                    move_and_click(1270, 60)
+                    pyautogui.hotkey("ctrl", "a")
+                    time.sleep(0.5)
+                    pyautogui.hotkey("ctrl", "v")
+                    pyautogui.press("enter")
 
-        pyautogui.press("F5")
-        time.sleep(delay)  # 指定された遅延時間だけ待機
+                pyautogui.press("F5")
+                time.sleep(delay)  # 指定された遅延時間だけ待機
+        except Exception as e:
+            logger.error(f"Error opening page: {e}")
 
-    return web_count  # 開いたページの数を返す
+    return len(urls)  # 開いたページの数を返す
 
 
-def get_urls(url: str, web_open=False) -> list:
+def get_urls(url: str, web=False) -> list:
     response = requests.get(url)
     soup = BeautifulSoup(response.content, "html.parser")
     current_urls = [link.get("href") for link in soup.find_all("a") if link.get("href")]
-    if web_open:
-        if not isinstance(web_open, int):
-            web_open = 0
-        for url in current_urls:
-            open_page(url, delay=0)
+    if web:
+        if not isinstance(web, int):
+            web = 0
+        for current_url in current_urls:
+            open_page(current_url, delay=web)
     return current_urls
 
 
